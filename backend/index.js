44b9f8e39b71e34ff0deee0e3f8296abf53447b9
Ingi Þór Sigurðsson <ingi.sigurosson@aalto.fi>
2024-04-05 14:23:13 +0000

    Solve 3.17 and 3.18

--- backend/index.js
+++ backend/index.js
@@ -98,7 +98,7 @@ app.put("/api/persons/:id", (request, response, next) => {
         number: body.number
     };
 
-    Person.findByIdAndUpdate(request.params.id, note, { new: true })
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
